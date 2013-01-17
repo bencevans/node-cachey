bc9d2e345a59218214efd94271773ac879359924
Ben Evans <ben@bensbit.co.uk>
2013-01-17 09:09:46 +0000

    space indent

--- index.js
+++ index.js
@@ -5,66 +5,66 @@ exports = module.exports = createCachey;
 
 function createCachey (config) {
 
-	if(!config)
-		throw "You've not provided a redisClient instance";
+  if(!config)
+    throw "You've not provided a redisClient instance";
 
-	if(!config.preKey)
-		config.preKey = 'cachey:';
+  if(!config.preKey)
+    config.preKey = 'cachey:';
 
-	if(typeof config.redisClient == "undefined")
-		throw "You've not provided a redisClient instance";
+  if(typeof config.redisClient == "undefined")
+    throw "You've not provided a redisClient instance";
 
-	return new cachey(config);
+  return new cachey(config);
 
 }
 
 var cachey = function (config) {
-	this.preKey = config.preKey || 'cachey:';
-	this.redisClient = config.redisClient || config;
-	return this;
+  this.preKey = config.preKey || 'cachey:';
+  this.redisClient = config.redisClient || config;
+  return this;
 };
 
 cachey.prototype.cache = function (key, ttl, getDataFunction, returnDataFunction) {
-	var self = this;
+  var self = this;
 
-	// Get Data From DB
-	self.redisClient.get(self.preKey + key, function (err, data) {
-		if(err) return returnDataFunction(err);
+  // Get Data From DB
+  self.redisClient.get(self.preKey + key, function (err, data) {
+    if(err) return returnDataFunction(err);
 
-		// Data Exists? Return That!
-		if(data !== null) return returnDataFunction(null, data);
+    // Data Exists? Return That!
+    if(data !== null) return returnDataFunction(null, data);
 
-		// Get Data from Function
-		getDataFunction(function(err, data) {
-			if(err) return returnDataFunction(err);
+    // Get Data from Function
+    getDataFunction(function(err, data) {
+      if(err) return returnDataFunction(err);
 
-			// Save Data to DB
-			self.redisClient.set(self.preKey + key, data, function (err) {
-				if(err) return returnDataFunction(err);
+      // Save Data to DB
+      self.redisClient.set(self.preKey + key, data, function (err) {
+        if(err) return returnDataFunction(err);
 
-				// Set it to Expire with ttl provided
-				self.redisClient.expire(self.preKey + key, ttl, function(err) {
-					return returnDataFunction(err, data);
-				});
-			});
-		});
-	});
+        // Set it to Expire with ttl provided
+        self.redisClient.expire(self.preKey + key, ttl, function(err) {
+          return returnDataFunction(err, data);
+        });
+      });
+    });
+  });
 };
 
 cachey.prototype.flush = function (key, callback) {
-	var self = this;
+  var self = this;
 
-	if(typeof callback == 'undefined') {
-		callback = key;
-		key = '*';
-	}
-	this.redisClient.keys(this.preKey + key + '*', function(err, keys) {
-		async.map(keys, function(key, cb) {
-			self.redisClient.del(this.preKey + key, cb);
-		}, function(err, ress) {
-			if(err) return callback(err);
-			return callback(null, true);
-		});
-	});
+  if(typeof callback == 'undefined') {
+    callback = key;
+    key = '*';
+  }
+  this.redisClient.keys(this.preKey + key + '*', function(err, keys) {
+    async.map(keys, function(key, cb) {
+      self.redisClient.del(this.preKey + key, cb);
+    }, function(err, ress) {
+      if(err) return callback(err);
+      return callback(null, true);
+    });
+  });
 
 };
--- test.js
+++ test.js
@@ -6,59 +6,59 @@ var cachey = require('./')({redisClient:redisClient});
 
 describe("Cachey", function(){
 
-	it("shouldn't execute getDataFunction for a second time within the ttl and instead return cached data", function (done) {
+  it("shouldn't execute getDataFunction for a second time within the ttl and instead return cached data", function (done) {
 
-		this.timeout(10000);
+    this.timeout(10000);
 
-		var executed = 0;
+    var executed = 0;
 
-		cachey.cache('hello', 4, function(callback){
-			executed++;
-			callback(null, 'world');
-		}, function (err, data) {
+    cachey.cache('hello', 4, function(callback){
+      executed++;
+      callback(null, 'world');
+    }, function (err, data) {
 
-			cachey.cache('hello', 4, function(callback){
-				executed++;
-				callback(null, "world2");
-			}, function(err, data) {
-				assert.equal(1, executed);
-				assert.equal("world", data);
-				done();
-			});
+      cachey.cache('hello', 4, function(callback){
+        executed++;
+        callback(null, "world2");
+      }, function(err, data) {
+        assert.equal(1, executed);
+        assert.equal("world", data);
+        done();
+      });
 
-		});
+    });
 
-	});
+  });
 
-	it("should return getDataFunction errors to returnDataFunction", function (done) {
+  it("should return getDataFunction errors to returnDataFunction", function (done) {
 
-		this.timeout(5000);
+    this.timeout(5000);
 
-		cachey.cache('helloworld', 4, function(callback){
-			callback("BIGERROR", 'world');
-		}, function (err, data) {
-			assert.equal("BIGERROR", err);
-			done();
-		});
+    cachey.cache('helloworld', 4, function(callback){
+      callback("BIGERROR", 'world');
+    }, function (err, data) {
+      assert.equal("BIGERROR", err);
+      done();
+    });
 
-	});
+  });
 
-	it("should flush a key", function (done) {
+  it("should flush a key", function (done) {
 
-		this.timeout(5000);
+    this.timeout(5000);
 
-		cachey.cache('tmp', 60, function(cb){
-			cb(null, 'yo');
-		}, function(err, data) {
-			//assert(null, err);
-			assert('yo', data);
-			cachey.flush(function(err, ok){
-				assert.equal(null, err);
-				assert.equal(true, ok);
-				done();
-			});
-		});
-	});
+    cachey.cache('tmp', 60, function(cb){
+      cb(null, 'yo');
+    }, function(err, data) {
+      //assert(null, err);
+      assert('yo', data);
+      cachey.flush(function(err, ok){
+        assert.equal(null, err);
+        assert.equal(true, ok);
+        done();
+      });
+    });
+  });
 
 });
 
